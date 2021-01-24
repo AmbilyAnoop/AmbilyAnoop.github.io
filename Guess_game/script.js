ded533535dddf97122a08513e920ff1da3abde5f
AmbilyAnoop <ambily298@gmail.com>
2021-01-24 17:26:14 -0500

    updating files

--- Guess_game/script.js
+++ Guess_game/script.js
@@ -1,105 +1,80 @@
 'use strict';
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
 };
 
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
 
-for (const item of menu) console.log(item);
+  //if no guess
+  if (!guess) {
+    //document.querySelector('.message').textContent = 'No Number';
+    displayMessage('⛔No number');
+  }
 
-for (const [i, el] of menu.entries()) {
-  console.log(`${i + 1}: ${el}`);
-}
+  //if quess is secret Number
+  else if (guess == secretNumber) {
+    if (score > 1) {
+      //document.querySelector('.message').textContent = 'U win';
+      displayMessage('You won the game 🎉🎉');
+      document.querySelector('.number').textContent = secretNumber;
+      document.querySelector('body').style.backgroundColor = 'blue';
+      document.querySelector('.number').style.width = '30rem';
 
-// const game = {
-//   team1: 'Bayern Munich',
-//   team2: 'Borrussia Dortmund',
-//   players: [
-//     [
-//       'Neuer',
-//       'Pavard',
-//       'Martinez',
-//       'Alaba',
-//       'Davies',
-//       'Kimmich',
-//       'Goretzka',
-//       'Coman',
-//       'Muller',
-//       'Gnarby',
-//       'Lewandowski',
-//     ],
-//     [
-//       'Burki',
-//       'Schulz',
-//       'Hummels',
-//       'Akanji',
-//       'Hakimi',
-//       'Weigl',
-//       'Witsel',
-//       'Hazard',
-//       'Brandt',
-//       'Sancho',
-//       'Gotze',
-//     ],
-//   ],
-//   score: '4:0',
-//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-//   date: 'Nov 9th, 2037',
-//   odds: {
-//     team1: 1.33,
-//     x: 3.25,
-//     team2: 6.5,
-//   },
-// };
-// /*
-// We're building a football betting app (soccer for my American friends 😅)!
+      if (score > highScore) {
+        highScore = score;
+        document.querySelector('.highscore').textContent = highScore;
+      }
+    } else {
+      //document.querySelector('.message').textContent = 'You lost the game';
+      displayMessage('You Lost the game ..🛑Try Again');
+    }
+  }
+  //if guess is not secret Number
+  else if (guess != secretNumber) {
+    //if guess is too high
+    if (score > 1) {
+      displayMessage(
+        guess > secretNumber ? 'Number Too High 🌡' : 'Number Too Low 🌨'
+      );
+      {
+        //document.querySelector('.message').textContent = 'Too High';
+        //displayMessage('Number Too High 🌡');
+        score--;
+        document.querySelector('.score').textContent = score;
+      }
+    } else {
+      //document.querySelector('.message').textContent = 'You lost the game';
+      displayMessage('You Lost the game ..🛑Try Again');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  //if guess is too low
+  // else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     //document.querySelector('.message').textContent = 'Too Low';
+  //     displayMessage('Number Too Low 🌨');
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     //document.querySelector('.message').textContent = 'You lost the game';
+  //     displayMessage('You Lost the game ..🛑Try Again');
+  //   }
+});
 
-// Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
-
-// 1. Create one player array for each team (variables 'players1' and 'players2')
-// 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
-// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
-// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
-// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-// 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
-// 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
-
-// TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
-
-// GOOD LUCK 😀
-// */
-
-// // 1.
-// const [players1, players2] = game.players;
-// console.log(players1, players2);
-
-// // 2.
-// const [gk, ...fieldPlayers] = players1;
-// console.log(gk, fieldPlayers);
-
-// // 3.
-// const allPlayers = [...players1, ...players2];
-// console.log(allPlayers);
-
-// //4
-// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+//again
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  displayMessage('Start guessing ..');
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').textContent = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
